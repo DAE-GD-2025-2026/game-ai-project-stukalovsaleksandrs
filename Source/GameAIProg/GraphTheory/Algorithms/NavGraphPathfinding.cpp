8483793605294fac27a9a13230b62ab13c21f5bd
Aleksandrs Stukalovs <stukalovsaleksandrs@gmail.com>
2026-04-04 22:41:21 +0200

    W06: Implemented SSFA

--- Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
@@ -1,7 +1,5 @@
 ﻿#include "NavGraphPathfinding.h"
 
-#include <cassert>
-
 #include "AStar.h"
 #include "PathSmoothing.h"
 #include "VectorTypes.h"
@@ -11,85 +9,105 @@
 using namespace GameAI;
 
 std::vector<FVector2D> NavMeshPathfinding::FindPath(const FVector2D& startPos, const FVector2D& endPos,
-    NavGraph* const pNavGraph, std::vector<FVector2D>& debugNodePositions, std::vector<NavLine>& debugPortals) 
+	NavGraph* const pNavGraph, std::vector<FVector2D>& debugNodePositions, std::vector<NavLine>& debugPortals) 
 {
-    //Get the start and endTriangle
-    // How to get a triangle from a point?
-    auto& navPolygon{ *pNavGraph->GetNavPolygon() };
-    auto const pStartTriangle{ navPolygon.GetTriangleAtPosition(startPos, true) },
-        pEndTriangle{ navPolygon.GetTriangleAtPosition(endPos, true) };
+	//Create the path to return
+	std::vector<FVector2D> finalPath{};
 
-    // Points not in triangles -> no path
-    // NOTE: De Morgan's law: !A || !B = !(A && B)
-    if (!(pStartTriangle && pEndTriangle)) return {};
-    // Same triangle -> Moving directly
-    if (pStartTriangle == pEndTriangle) return {startPos, endPos};
-    
-    //We have valid start/end triangles, and they are not the same
-    //=> Start looking for a path
-    //Copy the graph
-    auto pNavGraphClone{ pNavGraph->Clone() };
-    
-    //Create Extra node for the Start and End nodes
-    auto addNode{
-        [&](FVector2D const& fromNodePos, TriPolygon::Triangle const& fromNodeTriangle)
-        {
-            // Adding node
-            int const fromNodeId{ pNavGraphClone->AddNode(std::make_unique<Node>(fromNodePos)) };
-            assert(fromNodeId >= 0);
-            // Adding connections
-            for (auto const& edge : fromNodeTriangle.GetEdges())
-            {
-                // 1. Getting a node from the edge
-                // 1.1. Getting edge idx
-                auto const edgeIdx { navPolygon.FindEdgeIndex(edge) };
-                check(edgeIdx.has_value());
-                // 1.2. Getting node from the edge idx
-                int const toNodeId{ pNavGraphClone->GetNodeIdFromEdgeIndex(edgeIdx.value()) };
-                if(toNodeId < 0) continue;// Not adding connections to edges with no nodes
-                // 2. Creating a connection and add it to the graph
-                auto pConnection{ std::make_unique<Connection>(fromNodeId, toNodeId) };
-                pConnection->SetWeight( FVector2D::Distance(
-                    pNavGraphClone->GetNode(fromNodeId)->GetPosition(),
-                    pNavGraphClone->GetNode(toNodeId)->GetPosition())
-                );
-                pNavGraphClone->AddConnection(
-                    std::move(pConnection)
-                );
-            }
-            
-            return fromNodeId;
-        }
-    };
-    int const startNodeId{ addNode(startPos, *pStartTriangle) },
-        endNodeId{ addNode(endPos, *pEndTriangle) };
-    
-    //Run A star on the new graph
-    AStar astar{ pNavGraphClone.get(), HeuristicFunctions::Euclidean };
-    auto nodePath{
-        astar.FindPath(
-            pNavGraphClone->GetNode(startNodeId).get(),
-            pNavGraphClone->GetNode(endNodeId).get())
-    };
-    
-    //Create the path to return
-    std::vector<FVector2D> pointPath;
-    pointPath.reserve(nodePath.size());
-    std::ranges::transform(nodePath, std::back_inserter(pointPath),
-                           [](auto const& node) { return node->GetPosition(); });
-    
-    //Debug Visualisation
-    // Extra: Run optimiser on new graph (First check if everything works without SSFA!)
-    debugPortals = SSFA::FindPortals(nodePath, *pNavGraph->GetNavPolygon());
-    pointPath = SSFA::OptimizePortals(debugPortals, *pNavGraph->GetNavPolygon());
-    
-    return pointPath;
+	//Get the start and endTriangle
+	auto navMeshPolygon = pNavGraph->GetNavPolygon();
+	
+	auto startTriangle = navMeshPolygon->GetTriangleAtPosition(startPos,true);
+	auto endTriangle = navMeshPolygon->GetTriangleAtPosition(endPos,true);
+	//We have valid start/end triangles and they are not the same
+	if (!startTriangle||!endTriangle)
+	{
+		return finalPath;
+	}
+	
+	if (*startTriangle == *endTriangle)
+	{
+		finalPath.emplace_back(startPos);
+		finalPath.emplace_back(endPos);
+		return finalPath;
+	}
+	//=> Start looking for a path
+	//Copy the graph
+	auto copiedGraph = pNavGraph->Clone();
+
+	//Create Extra node for the Start Node (Agent's position
+	auto startNode = std::make_unique<NavGraphNode>(startPos, -1);
+	int startNodeId = copiedGraph->AddNode(std::move(startNode));
+
+	auto edges = startTriangle->GetEdges();
+	
+	for (const auto& edge : edges)
+	{
+		auto edgeIndex = navMeshPolygon->FindEdgeIndex(edge);
+		if (!edgeIndex.has_value())
+		continue;
+		
+		int nodeId = copiedGraph->GetNodeIdFromEdgeIndex(edgeIndex.value());
+		
+		if (nodeId != Graphs::InvalidNodeId)
+		{
+			FVector2D nodePos = copiedGraph->GetNode(nodeId)->GetPosition();
+
+			copiedGraph->AddConnection(startNodeId, nodeId);
+			copiedGraph->AddConnection(nodeId, startNodeId);
+			
+		}
+		
+	}
+	
+	//Create extra node for the endNode
+	auto endNode = std::make_unique<NavGraphNode>(endPos, -1);
+	int endNodeId = copiedGraph->AddNode(std::move(endNode));
+
+	auto endEdges = endTriangle->GetEdges();
+	for (const auto& edge : endEdges)
+	{
+		auto edgeIndex = navMeshPolygon->FindEdgeIndex(edge);
+		if (!edgeIndex.has_value())
+		{
+			continue;
+		}
+		
+		int nodeId = copiedGraph->GetNodeIdFromEdgeIndex(edgeIndex.value());
+		if (nodeId != Graphs::InvalidNodeId)
+		{
+			FVector2D nodePos = copiedGraph->GetNode(nodeId)->GetPosition();
+			copiedGraph->AddConnection(endNodeId, nodeId);
+			copiedGraph->AddConnection(nodeId, endNodeId);
+		}
+		
+	}
+	copiedGraph->SetConnectionCostsToDistances();
+	
+	//Run A star on new graph
+	AStar pathfinder(copiedGraph.get(), HeuristicFunctions::Euclidean);
+	auto nodePath = pathfinder.FindPath(
+		copiedGraph->GetNode(startNodeId).get(),
+		copiedGraph->GetNode(endNodeId).get() );
+	
+	//Debug Visualisation
+	for (auto node : nodePath)
+	{
+		FVector2D pos = node->GetPosition();
+		finalPath.emplace_back(pos);
+		debugNodePositions.emplace_back(pos);
+	}
+	// Extra: Run optimiser on new graph (First check if everything works without SSFA!)
+	debugPortals = SSFA::FindPortals(nodePath, *pNavGraph->GetNavPolygon());
+	finalPath = SSFA::OptimizePortals(debugPortals, *pNavGraph->GetNavPolygon());
+	
+	return finalPath;
 }
 
 std::vector<FVector2D> NavMeshPathfinding::FindPath(const FVector2D& startPos, const FVector2D& endPos, NavGraph* const pNavGraph)
 {
-    std::vector<FVector2D> debugNodePositions{};
-    std::vector<NavLine> debugPortals{};
+	std::vector<FVector2D> debugNodePositions{};
+	std::vector<NavLine> debugPortals{};
 
-    return FindPath(startPos, endPos, pNavGraph, debugNodePositions, debugPortals);
+	return FindPath(startPos, endPos, pNavGraph, debugNodePositions, debugPortals);
 }
--- Source/GameAIProg/GraphTheory/Algorithms/PathSmoothing.h
+++ Source/GameAIProg/GraphTheory/Algorithms/PathSmoothing.h
@@ -2,133 +2,146 @@
 #include <vector>
 
 #include "NavGraphPathfinding.h"
-#include "Algo/Compare.h"
 #include "Movement/Pathfinding/Navmesh/TriPolygon.h"
 #include "Shared/Graph/Graph.h"
 #include "Shared/Graph/NavGraph/NavGraphNode.h"
 
 namespace GameAI
 {
-    class SSFA final
+	class SSFA final
+	{
+	public:
+
+		static std::vector<NavLine> FindPortals(std::vector<Node*> const& path, TriPolygon const& navPoly)
+		{
+			//Container
+			std::vector<NavLine> portals;
+
+			if (path.empty())
+				return portals;
+
+			// Adding the start portal
+			FVector2D const startPos = path.front()->GetPosition();
+			portals.emplace_back(NavLine{
+				.P1 = startPos,
+				.P2 = startPos
+			});
+
+			// Adding all the non-degenerate portals
+			// NOTE: Using a raw loop to be able to get access to the next element
+			for (int pathNodeIdx{}; pathNodeIdx < path.size(); ++pathNodeIdx)
+			{
+				// Getting the edge the node is on
+				Node* const pNode{ path.at(pathNodeIdx) };
+				int const edgeIdx{ static_cast<NavGraphNode*>(pNode)->GetEdgeIdx() };
+				if (edgeIdx == -1) continue;
+				TriPolygon::Edge edge{ navPoly.GetEdges().at(edgeIdx) };
+				
+				//Redetermine it's "orientation" based on the required path (left-right vs right-left) - p1 should be right point
+				FVector2D p1{ FVector2D(edge.GetP1(navPoly)) },
+					p2{ FVector2D(edge.GetP2(navPoly)) };
+
+				if (pathNodeIdx < path.size() - 1)
+				{
+					FVector2D const pathDirection{ path[pathNodeIdx + 1]->GetPosition() - pNode->GetPosition()},
+						edgeDirection{ p2 - p1 };
+
+					if (FVector2D::CrossProduct(pathDirection, edgeDirection) >= 0)
+						Swap(p1, p2);	
+				}
+				
+				//Store portal
+				portals.emplace_back(NavLine{
+					.P1 = p1,
+					.P2 = p2
+				});
+			}
+
+			//Add degenerate portal to force end evaluation
+			FVector2D const endPos = path.back()->GetPosition();
+			portals.emplace_back(NavLine{
+				.P1 = endPos,
+				.P2 = endPos 
+			});
+
+			return portals;
+		}
+
+static std::vector<FVector2D> OptimizePortals(std::vector<NavLine> const& portals, TriPolygon const& tryPolygon)
 {
-public:
-    //=== SSFA Functions ===
-    //--- References ---
-    //http://digestingduck.blogspot.be/2010/03/simple-stupid-funnel-algorithm.html
-    //https://gamedev.stackexchange.com/questions/68302/how-does-the-simple-stupid-funnel-algorithm-work
-    static std::vector<NavLine> FindPortals(std::vector<Node*> const & path, TriPolygon const & navPoly)
+    if (portals.empty()) return {};
+
+    FVector2D apex{ portals[0].P1 }, leftLeg{}, rightLeg{};
+    std::vector<FVector2D> path{};
+	path.reserve(portals.size());
+    path.push_back(apex);
+
+	// NOTE: Starting from 1, bc the first portal is already processed
+	size_t apexIdx{}, leftLegIdx{};
+    for (size_t portalIdx{ 1 }; portalIdx < portals.size(); ++portalIdx)
     {
-        //Container
-        // Are the lines from one portal node to another or how does it work?
-        // So, the first portal is degenerate, it is a starting point.
-        // Everything else is 
-        std::vector<NavLine> portals;
+	    NavLine const& portal{ portals.at(portalIdx) };
 
-        if (path.empty())
-            return portals;
-        
-        portals.reserve(path.size());
-        
-        //For each node received, get it's corresponding line
-        auto& edges{ navPoly.GetEdges() };
-        NavGraphNode const* pPreviousNavNode{};
-        for (auto const& node: path)
-        {
-            // Acquiring the edge
-            NavGraphNode const * const pNavNode{static_cast<NavGraphNode*>(node)};
-            auto const edgeIdx{ pNavNode->GetEdgeIdx() };
-            check(edgeIdx != -1);
-            auto const edge{ edges.at(edgeIdx) };
+    	// TODO: Make a versitile lambda
+    	//--- RIGHT CHECK ---
+    	//1. See if moving funnel inwards - RIGHT
+    	FVector2D const newRightLeg{ portal.P1 - apex };
+    	if (FVector2D::CrossProduct(rightLeg, newRightLeg) <= 0.f)
+    	{
+    		//2. See if new line degenerates a line segment - RIGHT
+    		if (FVector2D::CrossProduct(leftLeg, newRightLeg) < 0.f)
+    		{
+    			//Leftleg becomes new apex point
+    			apex += leftLeg;
+    			path.push_back(apex);
+    			apexIdx = leftLegIdx;
+    			//Calculate new legs (if not the end)
+    			if (portalIdx < portals.size() - 1)
+    			{
+    				portalIdx = apexIdx;
+    				rightLeg = portals.at(apexIdx).P1 - apex;
+    				leftLeg = portals.at(apexIdx).P2 - apex;
+    			}
+    			continue;
+    		}
+    		rightLeg = newRightLeg;
+    	}
 
-            //Redetermine it's "orientation" based on the required path (left-right vs right-left) - p1 should be right point
-            FVector2D p1{edge.GetP1(navPoly)}, p2{edge.GetP2(navPoly)};
-            if (pPreviousNavNode)// nullptr for the first one
-            {
-                FVector2D const pathDirection{ pNavNode->GetPosition() - pPreviousNavNode->GetPosition() },
-                    edgeDirection{ p2 - p1 };
-                if (FVector2D::CrossProduct(pathDirection, edgeDirection) > 0)
-                {
-                    Swap(p1, p2);
-                }
-            }
-            pPreviousNavNode = pNavNode;
-
-            //Store portal
-            portals.emplace_back( NavLine(
-                p1, p2
-            ));
-        }
-
-        //Add degenerate portal to force end evaluation
-        // No need, done in the main loop
-
-        return portals;
+    	//--- LEFT CHECK ---
+    	//1. See if moving funnel inwards - LEFT
+    	FVector2D const newLeftLeg{ portal.P2 - apex };
+    	if (FVector2D::CrossProduct(leftLeg, newLeftLeg) >= 0.f)
+    	{
+    		//2. See if new line degenerates a line segment - LEFT
+    		if (FVector2D::CrossProduct(leftLeg, rightLeg) < 0.f)
+    		{
+    			//Rightleg becomes new apex point
+    			apex += rightLeg;
+    			path.push_back(apex);
+    			apexIdx = leftLegIdx;
+    			//Calculate new legs (if not the end)
+    			if (portalIdx < portals.size() - 1)
+    			{
+    				portalIdx = apexIdx;
+    				leftLegIdx = apexIdx;
+    				rightLeg = portals.at(apexIdx).P1 - apex;
+    				leftLeg = portals.at(apexIdx).P2 - apex;
+    			}
+    			continue;
+    		}
+    		leftLeg = newLeftLeg;
+    		leftLegIdx = portalIdx;
+    	}
     }
 
-    static std::vector<FVector2D> OptimizePortals( std::vector<NavLine> const & portals, TriPolygon const & navPoly)
-    {
-        std::vector<FVector2D> path;
+	// Add last path point
+    path.push_back(portals.back().P1);
 
-        if (portals.empty()) return {};
-        
-        // Adding the first point
-        FVector2D apex{ portals.front().P1 };// P1 = P2
-        path.push_back(apex);
-        //P1 == right point of portal, P2 == left point of portal
-        FVector2D rightLeg{ portals.front().P1 - apex }, leftLeg{ portals.front().P2 - apex };
-        for (size_t portalIdx{ 1 }; portalIdx < portals.size(); ++portalIdx)// The first portal is already processed
-        {
-            NavLine const& portal{ portals.at(portalIdx) };
-            //--- RIGHT CHECK ---
-            //1. See if moving funnel inwards - RIGHT
-            FVector2D const newRight{ portal.P1 - apex };
-            if (FVector2D::CrossProduct(rightLeg, newRight) <= 0.f)
-            {
-                //2. See if new line degenerates a line segment - RIGHT
-                if (FVector2D::CrossProduct(rightLeg, leftLeg) < 0.f)
-                {
-                    //Leftleg becomes new apex point
-                    apex = leftLeg;
-                    path.push_back(apex);
-                    //Calculate new legs (if not the end)
-                    if (portalIdx < portals.size() - 1)
-                    {
-                        rightLeg = portals.at(portalIdx).P1 - apex;
-                        leftLeg = portals.at(portalIdx).P2 - apex;
-                        continue;
-                    }
-                }
-            }
+    return path;
+}
 
-            //--- LEFT CHECK ---
-            //1. See if moving funnel inwards - LEFT
-            FVector2D const newLeft{ portal.P2 - apex };
-            if (FVector2D::CrossProduct(leftLeg, newLeft) <= 0.f)
-            {
-                //2. See if new line degenerates a line segment - LEFT
-                if (FVector2D::CrossProduct(leftLeg, rightLeg) < 0.f)
-                {
-                    //Rightleg becomes new apex point
-                    apex = rightLeg;
-                    path.push_back(apex);
-                    //Calculate new legs (if not the end)
-                    if (portalIdx < portals.size() - 1)
-                    {
-                        rightLeg = portals.at(portalIdx).P1 - apex;
-                        leftLeg = portals.at(portalIdx).P2 - apex;
-                        continue;
-                    }
-                }
-            }
-        }
-        
-        // Add last path point
-        path.push_back(portals.back().P1);
-
-        return path;
-    }
-private:
-    SSFA() {};
-    ~SSFA() {};
-};
+	private:
+		SSFA() {};
+		~SSFA() {};
+	};
 }
