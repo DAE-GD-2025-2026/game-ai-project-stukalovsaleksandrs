bee428ebb88139e39b22526ea753c8d93f90aaab
Aleksandrs Stukalovs <stukalovsaleksandrs@gmail.com>
2026-04-04 19:57:59 +0200

    W06: Added FindPortals

--- Source/GameAIProg/GraphTheory/Algorithms/PathSmoothing.h
+++ Source/GameAIProg/GraphTheory/Algorithms/PathSmoothing.h
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "NavGraphPathfinding.h"
+#include "Algo/Compare.h"
 #include "Movement/Pathfinding/Navmesh/TriPolygon.h"
 #include "Shared/Graph/Graph.h"
 #include "Shared/Graph/NavGraph/NavGraphNode.h"
@@ -15,20 +16,53 @@ public:
     //--- References ---
     //http://digestingduck.blogspot.be/2010/03/simple-stupid-funnel-algorithm.html
     //https://gamedev.stackexchange.com/questions/68302/how-does-the-simple-stupid-funnel-algorithm-work
-    static std::vector<NavLine> FindPortals(std::vector<Node*> const & Path, TriPolygon const & NavPoly)
+    static std::vector<NavLine> FindPortals(std::vector<Node*> const & path, TriPolygon const & navPoly)
     {
         //Container
-        std::vector<NavLine> Portals = {};
+        // Are the lines from one portal node to another or how does it work?
+        // So, the first portal is degenerate, it is a starting point.
+        // Everything else is 
+        std::vector<NavLine> portals;
+
+        if (path.empty())
+            return portals;
+        
+        portals.reserve(path.size());
         
         //For each node received, get it's corresponding line
-        
+        auto& edges{ navPoly.GetEdges() };
+        NavGraphNode const* pPreviousNavNode{};
+        for (auto const& node: path)
+        {
+            // Acquiring the edge
+            NavGraphNode const * const pNavNode{static_cast<NavGraphNode*>(node)};
+            auto const edgeIdx{ pNavNode->GetEdgeIdx() };
+            check(edgeIdx != -1);
+            auto const edge{ edges.at(edgeIdx) };
+
             //Redetermine it's "orientation" based on the required path (left-right vs right-left) - p1 should be right point
+            FVector2D p1{edge.GetP1(navPoly)}, p2{edge.GetP2(navPoly)};
+            if (pPreviousNavNode)// nullptr for the first one
+            {
+                FVector2D const pathDirection{ pNavNode->GetPosition() - pPreviousNavNode->GetPosition() },
+                    edgeDirection{ p2 - p1 };
+                if (FVector2D::CrossProduct(pathDirection, edgeDirection) > 0)
+                {
+                    Swap(p1, p2);
+                }
+            }
+            pPreviousNavNode = pNavNode;
 
             //Store portal
+            portals.emplace_back( NavLine(
+                p1, p2
+            ));
+        }
 
         //Add degenerate portal to force end evaluation
+        // No need, done in the main loop
 
-        return Portals;
+        return portals;
     }
 
     static std::vector<FVector2D> OptimizePortals( std::vector<NavLine> const & Portals, TriPolygon const & NavPoly)
